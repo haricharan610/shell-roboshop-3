848bb30aaaaacd4addb4f1f711fc941f87389e3d
haricharan610 <haricharan610@gmail.com>
2026-08-17 17:02:56 +0530

    cpu-usage.sh

--- disk-uasage.sh
+++ disk-uasage.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-DISK_USAGE=(df -ht | grep -v Filesystem)
+DISK_USAGE=$(df -ht | grep -v Filesystem)
 
-DISK_THRESHHOLD=1 ## 75% minum usage
+DISK_THRESHOLD=75
 MSG=""
-IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)##IP → fetches instance private IP (works in AWS EC2)
-while IFS=read line ## Reads each line from DISK_USAGE
+
+IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
+while IFS= read -r line
 do
-   USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)   ###$6--> use cloumns eg-75%
-   PARTITION=$(echo $line | awk '{print $7F}') ## $7 --> mount point
-   if [ $USAGE -ge $DISK_THRESHHOLD ]  ## trigger ALert
-   then
-     MSG+="HIGH Disk usage on $PARTITION: $USAGE %<br>" ### <br> represents HTML new line
-   fi
-done  <<< $DISK_USAGE
+    USAGE=$(echo "$line" | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo "$line" | awk '{print $7}')
+
+    if [ "$USAGE" -ge "$DISK_THRESHOLD" ]; then
+        MSG+="HIGH Disk usage on $PARTITION: $USAGE %<br>"
+    fi
+
+done <<< "$DISK_USAGE"
+
+echo "$MSG"
